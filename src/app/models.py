12c5dc1eaf435301486738344c5cc5d620637763
Eliran Boraks <eboraks@gmail.com>
2023-08-20 12:02:41 -0400

    Adding ItemVector to models and fixing non-null fields on Document

--- src/app/models.py
+++ src/app/models.py
@@ -9,18 +9,18 @@ from datetime import datetime
 class Bookmark(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     url: str
-    update_at: datetime.datetime = Field(
+    update_at: datetime = Field(
         default_factory=datetime.utcnow, nullable=False)
     document_id: Optional[int] = Field(default=None, nullable=True)
 
 
 class Document(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    title: str
+    title: str = Field(default=None)
     url: str
     authors: List[float] = Field(sa_column=Column(ARRAY(Float)))
-    summary_generated: str
-    publication_date: datetime.datetime
+    summary_generated: str = Field(default=None)
+    publication_date: datetime = Field(default=None)
 
 
 class Keyphrase(SQLModel, table=True):
@@ -40,15 +40,22 @@ class Keyphrase(SQLModel, table=True):
 class WD_Item(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     label: Optional[str] = Field(default=None)
-    description = Optional[TEXT] = Field(default=None)
-    wikipedia_page_id: Optional[int]
-    views: Optional[int]
-    links: Optional[int]
+    description: Optional[str] = Field(default=None)
+    wikipedia_page_id: Optional[int] = Field(default=None)
+    views: Optional[int] = Field(default=None)
+    links: Optional[int] = Field(default=None)
 
 
 class WD_Alias(SQLModel, table=True):
-    id = Optional[int] = Field(default=None, primary_key=True)
-    alias = Optional[str] = Field(default=None)
-    lcase_alias = Optional[str] = Field(default=None)
+    id: Optional[int] = Field(default=None, primary_key=True)
+    alias: Optional[str] = Field(default=None)
+    lcase_alias: Optional[str] = Field(default=None)
 
-# engine = create_engine('postgresql+psycopg2://app:2214@localhost/icognition')
+
+class WD_ItemVector(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    text: Optional[str] = Field(default=None)
+    wikidata_id: Optional[int] = Field(default=None)
+    text_vec: List[float] = Field(sa_column=Column(Vector(384)))
+    update_at: datetime = Field(
+        default_factory=datetime.utcnow, nullable=False)
