b911afb956fd70f4abda9fd8193c1969ae8f95dd
Eliran Boraks <eboraks@gmail.com>
2023-09-06 07:43:00 -0400

    Deleting pointer to specific class/modules

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,3 @@
-#from .icog_util import *
-#from .keyphrase_extractor import *
-__all__ = ['icog_util', 'keyphrase_extractor', 'html_parser']
+# from .icog_util import *
+# from .keyphrase_extractor import *
+# __all__ = ['icog_util', 'keyphrase_extractor', 'html_parser']
