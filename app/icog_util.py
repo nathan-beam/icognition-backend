3e0a07e2e8930b394b012908fb26b123ebab838d
Eliran Boraks <eboraks@gmail.com>
2023-09-06 07:45:46 -0400

    Adding Sumy code that uses LSA to shorten text

--- app/icog_util.py
+++ app/icog_util.py
@@ -1,8 +1,18 @@
-from stop_words import get_stop_words
-from nltk.tokenize import word_tokenize
 import logging
 import sys
 import string
+import numpy as np
+import math
+
+# from stop_words import get_stop_words
+from nltk.tokenize import word_tokenize
+from transformers import AutoTokenizer
+from typing import Any, Dict, List, Union
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lsa import LsaSummarizer as Summarizer
+from sumy.nlp.stemmers import Stemmer
+from sumy.utils import get_stop_words
 
 
 logging.basicConfig(stream=sys.stdout, format='%(asctime)s - %(message)s',
@@ -29,6 +39,40 @@ def remove_stop_words(string, return_format='String'):
         return filtered_tokens
     else:
         return ' '.join(filtered_tokens)
+
+
+# Write recursive function that tokenizes and splits the text into chunks of 512 tokens
+# Then, use the summarizer to summarize each chunk
+def truncate_text(text: str, tokenizer: AutoTokenizer, summarizer: Summarizer, LANGUAGE="english") -> str:
+    max_length = tokenizer.model_max_length
+    tokens_ids = tokenizer(text, return_tensors="pt").input_ids
+    num_tokens = len(tokens_ids[0])
+
+    if (num_tokens > max_length):
+        logging.info(
+            f"Text is too long. Splitting into chunks of {max_length} tokens")
+        parser = PlaintextParser.from_string(text, Tokenizer(LANGUAGE))
+        num_sentences = len(parser.document.sentences)
+        avg_tokens_per_sentence = int(num_tokens / num_sentences)
+        excess_tokens = (num_tokens - max_length)
+        num_sentences_to_summarize = num_sentences - \
+            (math.ceil(excess_tokens / avg_tokens_per_sentence))
+
+        logging.info(f"Number of tokens: {num_tokens}.")
+        logging.info(f"Number of sentences: {num_sentences}.")
+        logging.info(
+            f"Average tokens per sentence: {avg_tokens_per_sentence}.")
+        logging.info(f"Excess tokens: {excess_tokens}.")
+        logging.info(
+            f"Number of sentences to summarize: {num_sentences_to_summarize}")
+
+        summary = summarizer(parser.document, num_sentences_to_summarize)
+        summary_text = " ".join([sentence._text for sentence in summary])
+        summarize_text(summary_text, tokenizer, summarizer, LANGUAGE)
+
+    else:
+        logging.info("Text is short enough. No need to summarizing.")
+        return text
 
 
 if __name__ == '__main__':
